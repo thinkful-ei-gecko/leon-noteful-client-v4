691181c95665104bbc008a0f86e7683119d6d30c
Leon Dail <leondailofficial@gmail.com>
2019-10-11 09:28:08 -0700

    prepared for deployment

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let config = {
   REACT_APP_DB_URL: "http://localhost:8000/noteful/api",
-  REACT_APP_API_KEY: "7abf6605-f5f9-454f-8972-1ec4079180ca"
+  REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 }
 
 export default config;
