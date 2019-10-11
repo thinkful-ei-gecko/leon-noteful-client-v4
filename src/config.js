f532071584d0abed054a8b156e8af54b0648d2ac
Leon Dail <leondailofficial@gmail.com>
2019-10-11 11:39:28 -0700

    finished deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 let config = {
-  REACT_APP_DB_URL: "http://localhost:8000/noteful/api",
+  REACT_APP_DB_URL: "https://nameless-fjord-82718.herokuapp.com/noteful/api",
   REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 }
 
